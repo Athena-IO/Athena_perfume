4c93f6cbaa87c8315cece325316c1907054935ed
Mohammad Hossein Javanmardinia <mmdjvnm@gmail.com>
2025-11-04 20:39:17 +0330

    models.py

--- Athena_perfume/athenaperfume/backend/accounts/models.py
+++ Athena_perfume/athenaperfume/backend/accounts/models.py
@@ -2,9 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    is_wholesaler = models.BooleanField(default=False)#مشتری عمده فروش است یا خیر؟
+    is_wholesaler = models.BooleanField(default=False)  # مشتری عمده فروش است یا خیر؟
     company_name = models.CharField(max_length=200, blank=True)
     phone = models.CharField(max_length=20, blank=False)
- 
-def __str__(self):
- return f'{self.company_name}'
+
+    def __str__(self):
+        return f"{self.company_name or self.username}"
+

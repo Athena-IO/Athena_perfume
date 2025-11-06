c0e8b073692e200d65b3af1a7812553e59cd3c90
fatemeh <fatemee.yazdaniiii4@gmail.com>
2025-11-06 16:01:15 +0330

    Remove role from registration serializer to enforce default 'user' role

--- Athena_perfume/athenaperfume/backend/accounts/serializers.py
+++ Athena_perfume/athenaperfume/backend/accounts/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from .models import CustomUser
-
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'phone', 'password', 'role']
+        fields = ['username', 'email', 'phone', 'password']  
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = CustomUser(**validated_data)
         user.set_password(password)
+        user.role = 'user'  
         user.save()
         return user

6ae117eecbd64d5c9f601c3ece8a914dcbaf329d
pars iran <mmdjvnm@gmail.com>
2025-11-17 21:30:40 +0330

    update

--- Athena_perfume/athenaperfume/backend/accounts/models.py
+++ Athena_perfume/athenaperfume/backend/accounts/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
+
 
 class CustomUser(AbstractUser):
     # optional phone field (unique)
@@ -13,3 +15,23 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return f"{self.username or self.phone} ({self.role})"
+
+
+# -------------------------
+# مدل ثبت لاگ‌های ورود
+# -------------------------
+class LoginLog(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    username_attempt = models.CharField(max_length=150)
+    ip_address = models.CharField(max_length=50)
+    user_agent = models.TextField()
+    success = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username_attempt} - {'Success' if self.success else 'Failed'} - {self.timestamp}"

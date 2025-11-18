5b609a351fad3ab68dc0920bb6ef495e299250b7
pars iran <mmdjvnm@gmail.com>
2025-11-18 18:41:58 +0330

    Add accounts API URLs and update main urls

--- Athena_perfume/athenaperfume/backend/athena/urls.py
+++ Athena_perfume/athenaperfume/backend/athena/urls.py
@@ -5,8 +5,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-    path('api/products/', include('products.urls')),  
+    path('api/accounts/', include('accounts.urls')),  # register, me, login-logs و ...
+    path('api/products/', include('products.urls')),
+
+    # این سه تا باعث میشه مستقیم این آدرس‌ها کار کنن:
+    # → http://127.0.0.1:8000/login/
+    # → http://127.0.0.1:8000/logout/
+    # → http://127.0.0.1:8000/refresh/
+    path('', include('accounts.urls_api')),
 ]
 
 if settings.DEBUG:

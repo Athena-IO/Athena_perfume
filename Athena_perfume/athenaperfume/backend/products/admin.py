2061bd18fa9ba8b80b9383b1dd68b68c6ca42490
fatemeh <fatemee.yazdaniiii4@gmail.com>
2025-12-01 16:29:12 +0330

    Add admin.py for products app and register models

--- Athena_perfume/athenaperfume/backend/products/admin.py
+++ Athena_perfume/athenaperfume/backend/products/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
+# products/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from .models import Brand, Category, TagGroup, Tag, Product
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(TagGroup)
+class TagGroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'color', 'icon']
+    search_fields = ['name']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'group']
+    list_filter = ['group']
+    search_fields = ['name']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'price', 'stock', 'is_active']
+    list_filter = ['brand', 'category', 'gender', 'is_active']
+    search_fields = ['name', 'brand__name']
+    prepopulated_fields = {'slug': ('name',)}
+    filter_horizontal = ['tags']  

e362b5852568c4c085ebf1eaa4fd92ad8ebc98f1
fatemeh <fatemee.yazdaniiii4@gmail.com>
2025-12-01 16:27:02 +0330

    Fixed merge conflict in athena/urls.py

--- Athena_perfume/athenaperfume/backend/athena/urls.py
+++ Athena_perfume/athenaperfume/backend/athena/urls.py
@@ -5,27 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD:athena/urls.py
-<<<<<<< HEAD
-    path('accounts/', include('accounts.urls')),   # routes related to auth
-    path('', include('shop.urls')),                # shop main pages
-=======
     path('api/accounts/', include('accounts.urls')),
-    path('api/products/', include('products.urls')), 
->>>>>>> f269ab4 (Initial commit - Django project with MySQL setup)
-=======
-    path('api/accounts/', include('accounts.urls')),
-    path('api/products/', include('products.urls')), 
->>>>>>> 543747319162a07cf0de1453ae58761b9b339832:Athena_perfume/athenaperfume/backend/athena/urls.py
+    path('api/products/', include('products.urls')),   
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-<<<<<<< HEAD:athena/urls.py
-<<<<<<< HEAD
-=======
-
->>>>>>> f269ab4 (Initial commit - Django project with MySQL setup)
-=======
-
->>>>>>> 543747319162a07cf0de1453ae58761b9b339832:Athena_perfume/athenaperfume/backend/athena/urls.py
